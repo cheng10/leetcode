b6a117ab99b4e071d832496ee604dc5f66dbb309
cheng10 <cheng10@ualberta.ca>
2016-10-19 00:15:27 -0600

    remove-nth-node-from-edn-of-list v2

--- remove_nth_node_from_end_of_list.py
+++ remove_nth_node_from_end_of_list.py
@@ -11,10 +11,13 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        dummy=ListNode(0); dummy.next=head
+        dummy=ListNode(0)
+        dummy.next=head
         p1=p2=dummy
-        for i in range(n): p1=p1.next
+        for i in range(n): 
+            p1=p1.next
         while p1.next:
-            p1=p1.next; p2=p2.next
+            p1=p1.next
+            p2=p2.next
         p2.next=p2.next.next
         return dummy.next
